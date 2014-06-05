4d4089c7e73f272bab3927004e56fde1e1ffdc06
Artsuu_Tsai <Artsuu_Tsai@169.254.133.191>
2014-06-05 21:19:23 +0800

    update

--- src/testGit/test.java
+++ src/testGit/test.java
@@ -1,11 +1,58 @@
 package testGit;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+import sun.net.www.URLConnection;
+
 public class test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
- 
-		System.out.println("123 update");
+     
+		  System.out.println("123 update");
+		//
+		  
+		 String sURL = "https://www.google.com.tw/?gws_rd=cr&ei=xjNrU8SvNYbkkgXhjYH4Bg#q=123";
+		    System.out.println(sURL);
+		    URL url = new URL(sURL);
+		    HttpURLConnection httpCon = (HttpURLConnection) url.openConnection();
+		    //set http request headers
+		            httpCon.addRequestProperty("Host", "https://www.google.com.tw/?gws_rd=cr&ei=xjNrU8SvNYbkkgXhjYH4Bg#q=123");
+		            httpCon.addRequestProperty("Connection", "keep-alive");
+		            httpCon.addRequestProperty("Cache-Control", "max-age=0");
+		            httpCon.addRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+		            httpCon.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36");
+		            httpCon.addRequestProperty("Accept-Encoding", "gzip,deflate,sdch");
+		            httpCon.addRequestProperty("Accept-Language", "en-US,en;q=0.8");
+		            //httpCon.addRequestProperty("Cookie", "JSESSIONID=EC0F373FCC023CD3B8B9C1E2E2F7606C; lang=tr; __utma=169322547.1217782332.1386173665.1386173665.1386173665.1; __utmb=169322547.1.10.1386173665; __utmc=169322547; __utmz=169322547.1386173665.1.1.utmcsr=stackoverflow.com|utmccn=(referral)|utmcmd=referral|utmcct=/questions/8616781/how-to-get-a-web-pages-source-code-from-java; __gads=ID=3ab4e50d8713e391:T=1386173664:S=ALNI_Mb8N_wW0xS_wRa68vhR0gTRl8MwFA; scrElm=body");
+		            HttpURLConnection.setFollowRedirects(false);
+		            httpCon.setInstanceFollowRedirects(false);
+		            httpCon.setDoOutput(true);
+		            httpCon.setUseCaches(true);
+
+		            httpCon.setRequestMethod("GET");
+
+		            BufferedReader in = new BufferedReader(new InputStreamReader(httpCon.getInputStream(), "UTF-8"));
+		            String inputLine;
+		            StringBuilder a = new StringBuilder();
+		            int controlwrtiesignal=0;
+		            while ((inputLine = in.readLine()) != null){
+		            	if(inputLine.contains("<p")){controlwrtiesignal=1;}
+		            if(controlwrtiesignal==1){	
+		            System.out.println(inputLine);
+		            }
+		            if(inputLine.contains("/p>"))
+		            	controlwrtiesignal=0;
+		            }
+		            in.close();
+
 		
 	} 
 
